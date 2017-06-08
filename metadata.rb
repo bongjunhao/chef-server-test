e939b18efe177d23fb62463467f8fda691a71c18
Bong Jun Hao <bongjunhao@gmail.com>
2017-06-08 11:19:30 +0800

    New repo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures chef-server-test'
 long_description 'Installs/Configures chef-server-test'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
