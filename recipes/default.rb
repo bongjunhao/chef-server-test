6a4cc55340eb389776d2733e83781c2cb61e4fb6
Bong Jun Hao <bongjunhao@gmail.com>
2017-06-08 10:01:48 +0800

    First commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,54 @@
 #
-# Cookbook:: chef-server-test
+# Cookbook Name:: chef-server
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package_url = node['chef-server']['url']
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+#attributes to create admin user
+userName = node['userName']
+firstName = node['firstName']
+lastName = node['lastName']
+email = node['email']
+password = node['password']
+privateKeyName = node['privateKeyName']
+
+#attributes to create organization
+shortName = node['shortName']
+fullName = node['fullName']
+orgPrivateKeyName = node['orgPrivateKeyName']
+
+
+# package is remote, download it to '/tmp/kitchen/cache/chef-server-core-12.15.7-1.el7.x86_64.rpm'
+remote_file package_local_path do
+  source package_url
+end
+
+#Install the package and execute 'chef-server-ctl reconfigure' everytime installation is done
+package package_name do
+  source package_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[reconfigure-chef-server]', :immediately
+  notifies :run, 'execute[create-admin-user]', :immediately
+  notifies :run, 'execute[create-organization]', :immediately
+end
+
+# reconfigure the installation, do not execute if no installation done (i.e chef-server is already installed) - ensures idempotency
+execute 'reconfigure-chef-server' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
+
+#Create one admin user
+execute 'create-admin-user' do
+  command "sudo chef-server-ctl user-create #{userName} #{firstName} #{lastName} #{email} #{password} --filename #{privateKeyName}.pem"
+  action :nothing
+end
+
+#Create the initial organization
+execute 'create-organization' do
+  command "sudo chef-server-ctl org-create #{shortName} #{fullName} --association #{userName} --filename #{orgPrivateKeyName}.pem"
+  action :nothing
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,15 +4,36 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+#
+# unless os.windows?
+#   # This is an example test, replace with your own test.
+#   describe user('root'), :skip do
+#     it { should exist }
+#   end
+# end
+#
+# # This is an example test, replace it with your own test.
+# describe port(80), :skip do
+#   it { should_not be_listening }
+# end
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+#Test that the chef-server installation package has been downloaded
+describe file("/tmp/kitchen/cache/chef-server-core-12.15.7-1.el7.x86_64.rpm") do
+  it {should exist}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+#Ensure that the chef-server package has been installed
+describe package('chef-server-core') do
+  it {should be_installed}
+end
+
+#Check that the admin private key has been generated
+describe file("/admin.pem") do
+  it {should exist}
+end
+
+#Check that the organization private key has been created
+describe file("/orgPrivateKey.pem") do
+  it {should exist}
 end
